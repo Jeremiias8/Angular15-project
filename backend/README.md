ec71f575ac6a65617c723a78723192eb70172321
Jeremías <jeremiias122002@gmail.com>
2023-03-16 01:43:17 +0100

    food-order-project

---
+++ backend/README.md
@@ -0,0 +1,51 @@
+Setup Backend for Making Restful API
+
+1- Create backend folder
+2- npm initialization
+3- npm install typescript --save
+4- create tsconfig.json
+5- create .gitignore
+6- copy data.ts file to backend/src folder
+7- npm install express cors
+8- create server.ts
+    1- install @type
+    2- add apis
+9- add npm install nodemon ts-node --save-dev
+10- add urs.ts to frontend
+11- add httpclient module
+12- update food service
+
+--------------------------------------------------
+
+Login Page and Login API in NodeJS
+
+1- Create login components
+    1.1- Add to routes
+    1.2- Add TS Code
+    1.3- Add HTML code
+    1.4- Add reactive forms Module in App module
+    1.5- Add CSS code
+
+2- Make API for Login
+    2.1- Add dummy data in data.ts file
+    2.2- use JSON
+    2.3- Add JSONWebToken (JWT)
+    2.4- Test API using POSTMAN
+    
+--------------------------------------------------
+
+1- Test API
+2- Install ngx-toastr = Front part
+    2.1- Add library in App Module
+    2.2- Add BrowserAnimationsModule
+    2.3- Add toster CSS file in Angular.json file
+3- Create User Service
+    3.1- Create User Model for Login
+    3.2- Create IUserLogin Interface
+    3.3- Add User Subject in Service
+    3.4- Add Login Method to Login User
+    3.5- Add user url
+4- Add header in Login
+5- Add Local Storage method
+6- Add Logout Method
+    6.1- add it in header
---
+++ backend/src/data.ts
@@ -0,0 +1,119 @@
+
+export const sample_foods: any[] = [
+  {
+    id: '1',
+    name: 'Pizza 4 Quesos',
+    cookTime: '10-20',
+    price: 12,
+    favorite: false,
+    origins: ['Italy'],
+    stars: 4.5,
+    imageUrl: 'assets/img/food-1.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch']
+  },
+  {
+    id: '2',
+    name: 'Pepperoni',
+    cookTime: '20-30',
+    price: 14,
+    favorite: false,
+    origins: ['England'],
+    stars: 5,
+    imageUrl: 'assets/img/food-2.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch']
+  },
+  {
+    id: '3',
+    name: 'Margarita',
+    cookTime: '10-15',
+    price: 8,
+    favorite: false,
+    origins: ['Belgium'],
+    stars: 4.5,
+    imageUrl: 'assets/img/food-3.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch']
+  },
+  {
+    id: '4',
+    name: 'Carbonara',
+    cookTime: '15-25',
+    price: 11,
+    favorite: false,
+    origins: ['Greece'],
+    stars: 4.5,
+    imageUrl: 'assets/img/food-4.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch']
+  },
+  {
+    id: '5',
+    name: 'Pizza con tiras de Pollo y Carne picada',
+    cookTime: '25-35',
+    price: 16,
+    favorite: false,
+    origins: ['Argentina'],
+    stars: 4,
+    imageUrl: 'assets/img/food-5.jpg',
+    tags: ['FastFood', 'Pizza', 'Soup']
+  },
+  {
+    id: '6',
+    name: 'Pizza con tiras de Pollo y Carne picada',
+    cookTime: '35-50',
+    price: 10,
+    favorite: false,
+    origins: ['Norway'],
+    stars: 4.4,
+    imageUrl: 'assets/img/food-6.jpg',
+    tags: ['FastFood', 'Fry', 'Lunch']
+  },
+  {
+    id: '7',
+    name: 'Pizza con tiras de Pollo y Carne picada',
+    cookTime: '10-20',
+    price: 15,
+    favorite: false,
+    origins: ['Spain'],
+    stars: 4,
+    imageUrl: 'assets/img/food-7.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch']
+  },
+  {
+    id: '8',
+    name: 'Pizza con tiras de Pollo y Carne picada',
+    cookTime: '20-40',
+    price: 11,
+    favorite: false,
+    origins: ['Switzerland'],
+    stars: 5,
+    imageUrl: 'assets/img/food-8.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch']
+  }
+];
+
+export const sample_tags: any[] = [
+  { name: 'All', count: 8 },
+  { name: 'FastFood', count: 4 },
+  { name: 'Pizza', count: 3 },
+  { name: 'Lunch', count: 3 },
+  { name: 'SlowFood', count: 2 },
+  { name: 'Hamburguer', count: 2 },
+  { name: 'Fry', count: 1 },
+  { name: 'Soup', count: 1 },
+];
+
+export const sample_users: any[] = [
+  {
+    name: 'Jeremy Mancinelli',
+    email: 'jeremy@gmail.com',
+    password: 'fdgfdg123',
+    address: 'Ohio',
+    isAdmin: true
+  },
+  {
+    name: 'Jeritto Perri',
+    email: 'jeritto@gmail.com',
+    password: 'fdg123456',
+    address: 'Honolulu',
+    isAdmin: false
+  }
+];
